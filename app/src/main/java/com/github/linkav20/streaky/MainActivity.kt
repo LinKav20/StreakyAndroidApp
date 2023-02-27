a9eaa130d171dedb340762bc65a5c41dbc223eb1
LinKa <maltseva-as@mail.ru>
2023-02-27 23:41:50 +0300

    add dependencies and some blur experiments

--- app/src/main/java/com/github/linkav20/streaky/MainActivity.kt
+++ app/src/main/java/com/github/linkav20/streaky/MainActivity.kt
@@ -2,10 +2,21 @@ package com.github.linkav20.streaky
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
+import android.view.ViewOutlineProvider
+import com.github.linkav20.streaky.databinding.ActivityMainBinding
+import eightbitlab.com.blurview.RenderScriptBlur
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(ActivityMainBinding.inflate(layoutInflater).also { binding = it }.root)
+
+        binding.blurView.setupWith(binding.root, RenderScriptBlur(this)) // or RenderEffectBlur
+            .setBlurRadius(15f)
+            .setBlurAutoUpdate(true)
     }
 }
+
