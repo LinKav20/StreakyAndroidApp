2099d60a1b917c1bbf74aec9b7bcf0cd261222fc
LinKa <maltseva-as@mail.ru>
2023-03-02 18:14:58 +0300

    login layout

--- app/src/main/java/com/github/linkav20/streaky/MainActivity.kt
+++ app/src/main/java/com/github/linkav20/streaky/MainActivity.kt
@@ -1,22 +1,41 @@
 package com.github.linkav20.streaky
 
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
-import android.view.ViewGroup
-import android.view.ViewOutlineProvider
-import com.github.linkav20.streaky.databinding.ActivityMainBinding
-import eightbitlab.com.blurview.RenderScriptBlur
+import android.view.View
+import android.view.Window
+import android.view.WindowManager
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+import com.github.linkav20.streaky.databinding.LoginLayoutBinding
+
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: LoginLayoutBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(ActivityMainBinding.inflate(layoutInflater).also { binding = it }.root)
+        setContentView(LoginLayoutBinding.inflate(layoutInflater).also { binding = it }.root)
+        fullscreen()
 
-        binding.blurView.setupWith(binding.root, RenderScriptBlur(this)) // or RenderEffectBlur
-            .setBlurRadius(15f)
-            .setBlurAutoUpdate(true)
+
+       // binding.blurView.setupWith(binding.root, RenderScriptBlur(this)) // or RenderEffectBlur
+       //    .setBlurRadius(15f)
+       //    .setBlurAutoUpdate(true)
+    }
+
+    private fun fullscreen(){
+        supportActionBar?.hide()
+
+
+        val windowInsetsController =
+            WindowCompat.getInsetsController(window, window.decorView) ?: return
+        windowInsetsController.systemBarsBehavior =
+            WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        windowInsetsController.hide(WindowInsetsCompat.Type.systemBars())
     }
 }
 
