ae83c77e893ca9ff16001b9656f652ef43af6633
LinKa <maltseva-as@mail.ru>
2023-03-06 01:06:54 +0300

    add shadow to blur view

--- app/src/main/java/com/github/linkav20/streaky/MainActivity.kt
+++ app/src/main/java/com/github/linkav20/streaky/MainActivity.kt
@@ -2,22 +2,16 @@ package com.github.linkav20.streaky
 
 import android.annotation.SuppressLint
 import android.graphics.Color
-import android.os.Build
+import android.graphics.Outline
 import android.os.Bundle
 import android.view.View
 import android.view.ViewOutlineProvider
-import android.view.Window
 import android.view.WindowManager
-import android.widget.Toast
-import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
-import com.github.linkav20.streaky.databinding.ActivityMainBinding
-import com.github.linkav20.streaky.databinding.FragmentCreationTaskBinding
-import com.github.linkav20.streaky.databinding.FragmentLoginBinding
-import com.github.linkav20.streaky.databinding.FragmentSignupBinding
+import com.github.linkav20.streaky.databinding.*
 import eightbitlab.com.blurview.RenderScriptBlur
 
 
@@ -26,6 +20,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         window.setFlags(
             WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
             WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
@@ -49,15 +44,16 @@ class MainActivity : AppCompatActivity() {
     override fun onStart() {
         super.onStart()
         //fullscreen()
-        val blurs = arrayOf(
-            binding.fridayBlurview,
+       val blurs = arrayOf(
+            /*binding.fridayBlurview,
             binding.mondayBlurview,
             binding.tuesdayBlurview,
             binding.wednesdayBlurview,
             binding.thursdayBlurview,
             binding.saturdayBlurview,
-            binding.sundayBlurview,
-            binding.punishmentBlurview
+            binding.sundayBlurview,*/
+           binding.punishmentBlurview
+       //binding.blurView
         )
 
         for (blur in blurs) {
@@ -66,7 +62,12 @@ class MainActivity : AppCompatActivity() {
                 .setBlurRadius(25f)
                 .setBlurAutoUpdate(true)
             //blur.setBackgroundColor(R.color.lavender)
-            blur.outlineProvider = ViewOutlineProvider.BACKGROUND
+            blur.outlineProvider = object : ViewOutlineProvider() {
+                override fun getOutline(view: View?, outline: Outline) {
+                    blur.background.getOutline(outline)
+                    outline.alpha = 1f
+                }
+            }
             blur.clipToOutline = true
             blur.elevation = 10F
         }
